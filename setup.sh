ad1fe3920e74db435aab8c54026ba855ec3dbf26
ngoctd <trangoc.contact@gmail.com>
2022-08-15 18:12:08 +0700

    u

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 # nvim
 echo '====================' \
 && echo 'DOWNLOAD NVIM v0.7.2' \
-&& wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb \
+&& cd ~ && wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb \
 && echo '====================' \
 && echo 'INSTALL NVIM' \
 && sudo apt install ./nvim-linux64.deb \
+&& rm nvim-linux64.deb \
 && echo '====================' \
 && echo 'Config NVIM' \
+&& mkdir ~/.config/nvim && cd ~/.config/nvim && wget -O init.lua https://raw.githubusercontent.com/ngoctd314/dotfile/main/base-nvim.lua
 && echo '====================' \
 && echo 'Config TMUX' \
-&& rm nvim-linux64.deb \
-&& wget https://raw.githubusercontent.com/ngoctd313/dotfile/main/.tmux.conf \
+&& cd ~ && wget https://raw.githubusercontent.com/ngoctd313/dotfile/main/.tmux.conf \
