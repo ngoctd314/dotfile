aca15b92333cfa19373b881b6884830705d77cad
ngoctd <trangoc.contact@gmail.com>
2022-08-16 09:31:51 +0700

    u

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Copy and paste below code
 ```bash
 cd ~ \
 && wget https://raw.githubusercontent.com/ngoctd314/dotfile/main/setup.sh \
-&& chmod +x ./setup.sh
-&& ./setup.sh
-&& rm ./setup.sh
+&& chmod +x ./setup.sh \
+&& ./setup.sh \
+&& rm ./setup.sh \
 ```
--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 # nvim
 echo '====================' \
-&& echo 'DOWNLOAD NVIM v0.7.2' \
+&& echo 'Download NVIM v0.7.2' \
 && cd ~ && wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb \
 && echo '====================' \
-&& echo 'INSTALL NVIM' \
+&& echo 'Install NVIM' \
 && sudo apt install ./nvim-linux64.deb \
 && rm nvim-linux64.deb \
-&& echo '====================' \
-&& echo 'Config NVIM' \
 && rm -rf ~/.config/nvim && mkdir ~/.config/nvim && cd ~/.config/nvim && wget -O init.lua https://raw.githubusercontent.com/ngoctd314/dotfile/main/nvim.lua \
 && echo '====================' \
 && echo 'Config TMUX' \
 && cd ~ && wget https://raw.githubusercontent.com/ngoctd314/dotfile/main/.tmux.conf \
+&& echo '====================' \
+&& echo 'Install ZSH' \
+&& sudo apt install zsh \
+&& sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
+&& rm ~/.zshrc && cd ~ && wget https://raw.githubusercontent.com/ngoctd314/dotfile/main/.zshrc \
